d127b60df89ea8c8be6c8255314432d4f37cb56b
Jerome Serrano <jerome@placeiq.com>
2015-01-09 17:09:57 -0500

    Add hadoop settings to running script

--- bin/piqasus.sh
+++ bin/piqasus.sh
@@ -25,15 +25,21 @@ JAR=../target/Pegasus-1.3-SNAPSHOT-fatjar.jar
 WORK_DIR=pegasus_work
 
 N_NODES=$1
-N_REDUCERS=$2
+N_REDUCERS_RUNNER=$2
+N_REDUCERS_BLOCKS=$2
 INPUT_EDGES=$3
 BLOCK_SIZE=$4
 MAX_CONVERGENCE=$5
 MAX_ITERATIONS=$6
 
+HADOOP_OPTS_RUNNER='-D mapred.map.child.java.opts=-Xmx2048M -D io.sort.mb=1024 -D io.sort.record.percent=0.7 -D mapred.job.reuse.jvm.num.tasks=-1'
+HADOOP_OPTS_BLOCK="-D io.sort.mb=716 -D io.sort.record.percent=0.7 -D mapred.job.reuse.jvm.num.tasks=-1 -D mapred.job.reduce.input.buffer.percent=0.7"
+
+HADOOP="hadoop jar $JAR"
+
 hadoop dfs -rm -r $WORK_DIR
 
-hadoop jar $JAR com.placeiq.piqasus.InitialVectorGenerator $WORK_DIR/initial_vector $N_NODES $N_REDUCERS
-hadoop jar $JAR com.placeiq.piqasus.BlocksBuilder $WORK_DIR/initial_vector $WORK_DIR/vector_blocks $BLOCK_SIZE $N_REDUCERS vector
-hadoop jar $JAR com.placeiq.piqasus.BlocksBuilder $INPUT_EDGES $WORK_DIR/edge_blocks $BLOCK_SIZE $N_REDUCERS matrix
-hadoop jar $JAR com.placeiq.piqasus.Runner $WORK_DIR/edge_blocks $WORK_DIR/vector_blocks $WORK_DIR $N_REDUCERS $BLOCK_SIZE $MAX_CONVERGENCE $MAX_ITERATIONS
+$HADOOP com.placeiq.piqasus.InitialVectorGenerator $WORK_DIR/initial_vector $N_NODES $N_REDUCERS_BLOCKS
+$HADOOP com.placeiq.piqasus.BlocksBuilder $HADOOP_OPTS_BLOCK $WORK_DIR/initial_vector $WORK_DIR/vector_blocks $BLOCK_SIZE $N_REDUCERS_BLOCKS vector
+$HADOOP com.placeiq.piqasus.BlocksBuilder $HADOOP_OPTS_BLOCK $INPUT_EDGES $WORK_DIR/edge_blocks $BLOCK_SIZE $N_REDUCERS_BLOCKS matrix
+$HADOOP com.placeiq.piqasus.Runner $HADOOP_OPTS_RUNNER $WORK_DIR/edge_blocks $WORK_DIR/vector_blocks $WORK_DIR $N_REDUCERS_BLOCKS $BLOCK_SIZE $MAX_CONVERGENCE $MAX_ITERATIONS
